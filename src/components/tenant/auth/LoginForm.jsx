b20783d30a83da934e7fdd21e5bd95cb79e0a8ee
jasirpanghat <jazjasir7@gmail.com>
2025-08-19 17:16:48 +0530

    fix: navigte to Landingpage from Login Page

--- src/components/tenant/auth/LoginForm.jsx
+++ src/components/tenant/auth/LoginForm.jsx
@@ -229,7 +229,7 @@ const onSubmit = async (data) => {
           {/* Sign up link */}
           <p className="mt-8 text-center text-sm text-gray-600">
             Don't have an account?{" "}
-            <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+            <a href="/" className="font-medium text-blue-600 hover:text-blue-500">
               Sign up for free
             </a>
           </p>
